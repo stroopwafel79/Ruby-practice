4de0b0e8052b52700482a72de526921802513c8b
Kristi Lanzisera <kristilanzisera@Kristis-MacBook-Pro.local>
2019-04-08 14:32:23 -0700

    Added deaf_grandma function

--- first_file.rb
+++ first_file.rb
@@ -42,8 +42,8 @@ def sing_bottles_of_beer(number)
 	end
 end
 
-sing_bottles_of_beer(99)
-sing_bottles_of_beer(3)
+# sing_bottles_of_beer(99)
+# sing_bottles_of_beer(3)
 
 # Write a Deaf Grandma program. Whatever you say to grandma (whatever you type in), she should respond with  
 # HUH?!  SPEAK UP, SONNY!, unless you shout it (type in all capitals). If you shout, she can hear you 
@@ -55,5 +55,31 @@ sing_bottles_of_beer(3)
 # Hint 2: Try to think about what parts of your program should happen over and over again. All of 
 # those should be in your while loop.
 
+# Write a Deaf Grandma program. Whatever you say to grandma (whatever you type in), she should respond with  
+# HUH?!  SPEAK UP, SONNY!, unless you shout it (type in all capitals). If you shout, she can hear you 
+# (or at least she thinks so) and yells back, NO, NOT SINCE 1938! To make your program really believable, 
+# have grandma shout a different year each time; maybe any year at random between 1930 and 1950. 
+# (This part is optional, and would be much easier if you read the section on Ruby's random number 
+# 	generator at the end of the methods chapter.) You can't stop talking to grandma until you shout BYE.
+# Hint: Don't forget about  chomp!  'BYE'with an Enter is not the same as 'BYE' without one!
+# Hint 2: Try to think about what parts of your program should happen over and over again. All of 
+# those should be in your while loop.
 
+def deaf_grandma
+	puts "Say something to grandma."
+	response = gets.chomp
+	while response != "Bye"
+		if response == response.upcase
+			year = rand(1930..1951)
+			puts "NO, NOT SINCE #{year}!"
+			response = gets.chomp
+		else
+			puts "HUH?! SPEAK UP, SONNY!"
+			response = gets.chomp
+			
+		end
+	end
+end
+
+deaf_grandma
 
