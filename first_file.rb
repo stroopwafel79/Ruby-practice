3d4d929b35585f073a57b616698cc6bb32de93ca
Kristi Lanzisera <stroopwafel79@gmail.com>
2019-04-11 13:50:05 -0700

    Added OrangeTree class

--- first_file.rb
+++ first_file.rb
@@ -222,4 +222,71 @@ puts die.showing
 die.cheat 7
 puts die.showing
 
+########## Orange tree class ##########
 
+# Make an OrangeTree class. It should have a  height method which returns its height, and a  
+# oneYearPasses method, which, when called, ages the tree one year. Each year the tree grows 
+# taller (however much you think an orange tree should grow in a year), and after some number 
+# of years (again, your call) the tree should die. 
+#For the first few years, it should not produce 
+# fruit, but after a while it should, and I guess that older trees produce more each year than 
+# younger trees... whatever you think makes most sense. 
+# And, of course, you should be able to 
+# countTheOranges (which returns the number of oranges on the tree), and pickAnOrange (which reduces
+# the @orangeCount by one and returns a string telling you how delicious the orange was, or else it
+# just tells you that there are no more oranges to pick this year). Make sure that any oranges you 
+# don't pick one year fall off before the next year.
+
+class	OrangeTree
+
+	def initialize
+		@age = 0
+		@height = 0
+		@fruit = 0
+	end
+
+	def height
+		@height
+	end
+
+	def one_year_passes
+		@age += 1
+		@height += 2
+		@fruit = 0
+
+		if @age > 3 
+			@fruit += 2
+			if @age > 6
+				@fruit += 2
+			
+				if @age < 10
+					@fruit += 2
+				else
+					puts "Tree has died."
+					exit
+				end
+			end
+		end
+	end
+
+	def count_oranges
+		@fruit
+	end
+
+	def pick_orange
+		if @fruit > 0
+			@fruit -= 1
+			puts "That orange was soooo good!"
+		else
+			puts "There are no more oganges to pick this year."
+		end
+	end
+end
+
+tree = OrangeTree.new
+puts tree.count_oranges
+tree.one_year_passes
+tree.one_year_passes
+tree.one_year_passes
+puts tree.height
+puts tree.count_oranges
