546725eb00bff6831c268b7eace908a9b0c69601
Kristi Lanzisera <stroopwafel79@gmail.com>
2019-04-12 13:30:13 -0700

    Started Grandfather Clock problem

--- first_file.rb
+++ first_file.rb
@@ -290,3 +290,30 @@ tree.one_year_passes
 tree.one_year_passes
 puts tree.height
 puts tree.count_oranges
+
+
+############ Grandfather Clock ############
+
+# Grandfather Clock. Write a method which takes a block and calls it once for each hour 
+# that has passed today. That way, if I were to pass in the block do puts 'DONG!' end, it 
+# would chime (sort of) like a grandfather clock. Test your method out with a few different 
+# blocks (including the one I just gave you). Hint: You can use  Time.now.hour to get the 
+# current hour. However, this returns a number between 0 and 23, so you will have to alter 
+# those numbers in order to get ordinary clock-face numbers (1 to 12).
+
+
+def once_per_hour &block
+	current_hour = Time.now.hour
+
+	block.call
+	
+end
+
+
+
+
+
+
+
+
+
